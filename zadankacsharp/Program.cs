e358561d3b7373a698e56789acac5952ad9fea53
Nina Marcinkiewicz <nmarcinkiewicz@objectivity.co.uk>
2020-06-04 22:28:27 +0200

    dodanie weryfikacji zadania 2.5; nowa metoda

--- zadankacsharp/Program.cs
+++ zadankacsharp/Program.cs
@@ -10,8 +10,22 @@ namespace zadankacsharp
     {
         static void Main(string[] args)
         {
+            Zadanie24();
+            Zadanie25();
+        }
+
+        private static void Zadanie24()
+        {
             int x = 100;
             Console.WriteLine(++x * 2);
+            Console.ReadKey(); //202
+        }
+
+        private static void Zadanie25()
+        {
+            int x = 2, y = 3;
+            x *= y * 2; // x = x * y * 2 czyli 12
+            Console.WriteLine(x);
             Console.ReadKey();
         }
     }
