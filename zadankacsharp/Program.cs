ce5efefc831b75cdff541c96c1cdd42c8952dd57
Nina Marcinkiewicz <nmarcinkiewicz@objectivity.co.uk>
2020-06-04 22:43:33 +0200

    zmiana nazwy brancha; weryfikacja dla zadan 2.4 do 2.7

--- zadankacsharp/Program.cs
+++ zadankacsharp/Program.cs
@@ -10,8 +10,10 @@ namespace zadankacsharp
     {
         static void Main(string[] args)
         {
-            Zadanie24();
-            Zadanie25();
+            //Zadanie24();
+            //Zadanie25();
+            //Zadanie26();
+            Zadanie27();
         }
 
         private static void Zadanie24()
@@ -26,6 +28,27 @@ namespace zadankacsharp
             int x = 2, y = 3;
             x *= y * 2; // x = x * y * 2 czyli 12
             Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        private static void Zadanie26()
+        {
+            int x, y = 4;
+            x = (y -= 2);
+            Console.WriteLine(x);
+            x = y++;
+            Console.WriteLine(x);  //nie kapuje czemu tutaj jest 2
+            x = y--;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    
+        private static void Zadanie27()
+        {
+            bool x;
+            int y = 1, z = 1;
+            x = (y == 1 && z++ == 1);
+            Console.WriteLine(x); //true
             Console.ReadKey();
         }
     }
