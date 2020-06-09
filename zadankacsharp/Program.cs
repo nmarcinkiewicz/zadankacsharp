03d46d8c3dc298a943bcb4570593bf05abdc8d93
Nina Marcinkiewicz <nmarcinkiewicz@objectivity.co.uk>
2020-06-10 00:27:14 +0200

    draft rozwiązania - wersja basic nie działająca jeszcze

--- zadankacsharp/Program.cs
+++ zadankacsharp/Program.cs
@@ -10,7 +10,26 @@ namespace zadankacsharp
     {
         static void Main(string[] args)
         {
-         
+            //Napisz program, który sprawdza, czy podany rok jest rokiem przestępnym. Rok
+            //przestępny dzieli się bez reszty przez 4, nie dzieli się przez 100(za wyjątkiem lat podzielnych
+            //przez 400).//
+
+            int userInputYear;
+            Console.WriteLine("wprowadź rok w formacie YYYY");
+            userInputYear = Convert.ToInt32(Console.ReadLine());
+
+            if (userInputYear % 4 == 0)
+            {
+                if (!(userInputYear % 100 == 0))
+                {
+                    if (userInputYear % 400 ==0)
+                    {
+                        Console.WriteLine("Podany {0} rok jest rokiem przestępnym", userInputYear);
+                    }
+                }
+            }
+            Console.WriteLine("{0} nie jest rokiem przestępnym", userInputYear);
+            Console.ReadLine();
         }
     }
 }
