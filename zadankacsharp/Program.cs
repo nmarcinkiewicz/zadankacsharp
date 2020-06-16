7af4bbbcffe3719fcec33fc2201e2f3ee74c9426
Nina Marcinkiewicz <nmarcinkiewicz@objectivity.co.uk>
2020-06-16 23:56:19 +0200

    Rozwiązanie zdania 3.3. - potrzebny fix jakiś

--- zadankacsharp/Program.cs
+++ zadankacsharp/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +14,36 @@ namespace zadankacsharp
     {
         static void Main(string[] args)
         {
-         
+            Console.WriteLine("Podaj trzy liczby, każdą wprowadź w nowym wierszu");
+            var userInput1 = Console.ReadLine();
+            var userInput2 = Console.ReadLine();
+            var userInput3 = Console.ReadLine();
+
+            userInput1.Replace(",", ".");
+            userInput2.Replace(",", ".");
+            userInput3.Replace(",", ".");
+
+            SortedList sortedList = new SortedList();
+            if (double.TryParse(userInput1, out double result1))
+            {
+                sortedList.Add("1", userInput1);
+            }
+
+            if (double.TryParse(userInput2, out double result2))
+            {
+                sortedList.Add("2", userInput2);
+            }
+
+            if (double.TryParse(userInput3, out double result3))
+            {
+                sortedList.Add("3", userInput3);
+            }
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                Console.Write("{0}", sortedList);
+                Console.ReadKey();
+            }
         }
     }
 }
